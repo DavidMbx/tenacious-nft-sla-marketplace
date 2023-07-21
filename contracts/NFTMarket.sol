c91cc27725eb692f14cd96264cfc5f0a4601362e
Davide <davide.casuccio@gmail.com>
2023-07-21 10:53:45 +0200

    Altre modifiche

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+
+import "hardhat/console.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    //Su Solidity non possiamo avere lunghezza dinamica degli array quindi dobbiamo sapere la lunghezza
+    //Array di item comprati, item creati e item vendu
+    //Not sold= itemIds-itemSold 
+
+
+    uint256 listingPrice = 0.025 ether;
+    address payable owner;
+
+    constructor()  {
+      owner = payable(msg.sender);
+    }
+    //Owner e' colui che ha deployato il contratto, sarebbe il marketplace
+
+
+//Struct per ogni oggetto del market
+    struct MarketItem {
+      uint256 itemId;
+      address nftContract;
+      uint256 tokenId;
+      address payable seller;
+      address payable owner;
+      uint256 price;
+      bool sold;
+    }
+
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+    //Dato l'id di un item riesco a farmi ritornare tutto l'oggetto
+
+
+
+// Creo un oggetto evento per debugging
+    event MarketItemCreated (
+      uint256 indexed itemId,
+      address indexed nftContract,
+      uint256 indexed tokenId,
+      address payable seller,
+      address payable owner,
+      uint256 price,
+      bool sold
+    );
+
+
+    /* Updates the listing price of the contract */
+    function updateListingPrice(uint _listingPrice) public payable {
+      require(owner == msg.sender, "Only marketplace owner can update listing price.");
+      listingPrice = _listingPrice;
+    }
+
+    /* Returns the listing price of the contract */
+    function getListingPrice() public view returns (uint256) {
+      return listingPrice;
+    }
+
+    
+    //Funzione per creare un oggetto da zero e metterlo sul mercato
+    function createMarketItem(
+      address nftContract,
+      uint256 tokenId,
+      uint256 price
+    ) public payable nonReentrant {
+      require(price > 0, "Price must be at least 1 wei");
+      require(msg.value == listingPrice, "Price must be equal to listing price");
+
+      _itemIds.increment();
+      uint256 itemId=_itemIds.current();
+
+      idToMarketItem[tokenId] =  MarketItem(
+        itemId,
+        nftContract,
+        tokenId,
+        payable(msg.sender),
+        payable(address(this)),
+        price,
+        false
+      );
+
+      IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+
+      emit MarketItemCreated(
+        itemId,
+        nftContract,
+        tokenId,
+        payable(msg.sender),
+        payable(address(this)),
+        price,
+        false
+      );
+    }
+
+//Funzione di vendita di nft da un venditore ad un compratore
+    function createMarketSale(
+      address nftContract,
+      uint256 itemId
+    ) public payable nonReentrant{
+
+      uint price=idToMarketItem[itemId].price;
+      uint tokenId=idToMarketItem[itemId].tokenId;
+
+      require(msg.value==price, "Invia il prezzo richiesto per completare l'acquisto");
+
+      idToMarketItem[itemId].seller.transfer(msg.value);
+      IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+      idToMarketItem[itemId].owner=payable(msg.sender);
+      idToMarketItem[itemId].sold=true;
+      _itemsSold.increment();
+      payable(owner).transfer(listingPrice);
+    }
+
+//Funzione che ritorna gli nft di tutto il marketplace
+
+function fetchMarketItems() public view returns (MarketItem[] memory){
+  uint itemCount=_itemIds.current();
+  uint unsoldItemCount= _itemIds.current() -_itemsSold.current();
+  uint currentIndex=0;
+
+//Ho già a disposizione il numero di item presenti il tutto il marketplace
+  MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+  for(uint i=0; i< itemCount;i++){
+    if(idToMarketItem[i+1].owner==address(this)){
+      uint currentId = idToMarketItem[i+1].itemId;
+      MarketItem storage currentItem= idToMarketItem[currentId];
+      items[currentIndex]=currentItem;
+      currentIndex+=1;
+
+    }
+  }
+
+return items;
+}
+
+
+//Funzione che ritorna gli nft di un utente
+function fetchMyNfts() public view returns (MarketItem[] memory){
+  uint totalItemCount=_itemIds.current();
+  uint itemCount=0;
+  uint currentIndex=0;
+
+//Trovo il numero di nft posseduti da chi ha chiamato la funzione
+  for(uint i=0;i<totalItemCount;i++){
+    if(idToMarketItem[i+1].owner==msg.sender){
+      itemCount+=1;
+    }
+  }
+
+  //Posso creare l'array di item una volta saputa la lunghezza (non ho array dinamici)
+  MarketItem[] memory items= new MarketItem[](itemCount);
+  for(uint i=0; i<totalItemCount; i++){
+    if(idToMarketItem[i+1].owner==msg.sender){
+    uint currentId=idToMarketItem[i+1].itemId;
+    MarketItem storage currentItem=idToMarketItem[currentId];
+    items[currentIndex]=currentItem;
+    currentIndex+=1;
+  }
+  }
+  return items;
+}
+
+function fetchItemsCreated() public view returns (MarketItem[] memory) {
+
+  uint totalItemCount= _itemIds.current();
+  uint itemCount=0;
+  uint currentIndex=0;
+
+  for(uint i=0; i<totalItemCount; i++){
+    if(idToMarketItem[i+1].seller==msg.sender) {
+      itemCount += 1; 
+      }
+  }
+
+  MarketItem[] memory items=new MarketItem[](itemCount);
+  for(uint i=0; i<totalItemCount; i++){
+    if(idToMarketItem[i+1].seller==msg.sender){
+      uint currentId=idToMarketItem[i+1].itemId;
+      MarketItem storage currentItem=idToMarketItem[currentId];
+      items[currentIndex]=currentItem;
+      currentIndex +=1;
+    }
+  }
+  return items;
+
+}
+
+
+
+
+
+}
+
+
+
---
+++ contracts/NFT_Badge.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT_Badge is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("NFT Badge", "BDG") {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function burn(uint256 tokenId) external {
+        require(ownerOf(tokenId) == msg.sender, "Only the owner of the token can burn it.");
+        _burn(tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId,uint256 batchSize) internal override(ERC721) {
+        require(from == address(0) || to == address(0), "Non puo essere trasferito");
+        super._beforeTokenTransfer(from, to, tokenId,batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721URIStorage) {
+        super._burn(tokenId);
+        _burn(tokenId);
+    }
+}
